fc8e55f43eb2e4701f10febf18ea77d5c2e79141
Roonglit Chareonsupkul <roonglit@gmail.com>
2020-07-04 15:23:10 +0700

    Rubocop (#4)
    * apply rubocop
    
    * apply github actions
    
    * fix rubocop
    
    * fix for rubocop suggestions
    
    * fix rubocop

--- test/engine_new_test.rb
+++ test/engine_new_test.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
-class EngineNew::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, EngineNew
+module EngineNew
+  class Test < ActiveSupport::TestCase
+    test 'truth' do
+      assert_kind_of Module, EngineNew
+    end
   end
 end
